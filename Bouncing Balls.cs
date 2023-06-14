9d55d4816568324215424428498941e08877199d
Mohamed Aziz Ammar <108346635+aziz4ammar@users.noreply.github.com>
2023-06-14 23:14:37 +0100

    Final Commit

---
+++ Bouncing Balls.cs
@@ -0,0 +1,21 @@
+public class BouncingBall
+{
+    public static int bouncingBall(double h, double bounce, double window)
+    {
+        if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h)
+        {
+            return -1;
+        }
+
+        int count = 1;
+        double height = h * bounce;
+
+        while (height > window)
+        {
+            count += 2;
+            height *= bounce;
+        }
+
+        return count;
+    }
+}
